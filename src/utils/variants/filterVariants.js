6b9eeb36b4d77fbfc961257dc137d9590bc8d74e
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2021-10-31 12:27:33 +0100

    Update filterVariants to new JSON-Format

--- src/utils/variants/filterVariants.js
+++ src/utils/variants/filterVariants.js
@@ -2,9 +2,9 @@ const isEmptyObject = require("../json/jsonEmpty.js")
 
 function filterVariants(graphJSON, variants) {
     let isVariantsEmpty = isEmptyObject(variants);
-    filterVariantsConrete(graphJSON.dfg.graph, graphJSON.dfg.frequency, variants, isVariantsEmpty);
-    filterVariantsConrete(graphJSON.epc.graph, graphJSON.dfg.frequency, variants, isVariantsEmpty);
-    filterVariantsConrete(graphJSON.bpmn.graph, graphJSON.dfg.frequency, variants, isVariantsEmpty);
+    filterVariantsConrete(graphJSON.dfg.graph, variants, isVariantsEmpty);
+    //filterVariantsConrete(graphJSON.epc.graph, variants, isVariantsEmpty);
+    //filterVariantsConrete(graphJSON.bpmn.graph, variants, isVariantsEmpty);
 
     return graphJSON
 }
@@ -13,24 +13,34 @@ function arrayEquals(target, variants) {
     return target.some(v => variants.includes(v));
 }
 
-function filterVariantsConrete(graphJSONconcrete, frequency, variants, isVariantsEmpty) {
+function filterVariantsConrete(graphJSONconcrete, variants, isVariantsEmpty) {
+    let frequencyMap = getFrequencyMap(graphJSONconcrete[0]["data"]["variants"])
     for (var i = 0; i < graphJSONconcrete.length; i++) {
         let sum = 0;
         let graphData = graphJSONconcrete[i].data
-        let graphVariants = graphData.variants;
+        let graphVariants = Object.keys(graphData.variants);
         if (!(arrayEquals(graphVariants, variants)) && !isVariantsEmpty) {
             graphJSONconcrete.splice(i, 1);
             i--;
             continue;
         }
-        for (const [key, value] of Object.entries(frequency)) {
-            if (variants.includes(key)) {
+        for (const [key, value] of Object.entries(frequencyMap)) {
+            if (graphVariants.includes(key) && (variants.includes(key) || isVariantsEmpty)) {
                 sum += value;
             }
         }
         graphData.label = `${graphData.label} \n ${sum}`
-        //delete graphVariants
+        //delete graphJSONconcrete[i]["data"]["variants"]// Add this in Production Mode
     }
+}
+
+function getFrequencyMap(variants) {
+    let keys = Object.keys(variants);
+    let frequencyMap = {};
+    keys.forEach(function (key) {
+        frequencyMap[key] = Object.keys(variants[key]).length;
+    });
+    return frequencyMap;
 }
 
 module.exports = filterVariants;
