b5d669516743bfbf75791ab136480efaaa0d35c7
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2021-11-14 23:28:09 +0100

    Add variants count response to /ids

--- src/controller/crud/graphIds.js
+++ src/controller/crud/graphIds.js
@@ -1,18 +1,30 @@
 
-const logger = require('../../utils/log/log.js'); 
+const logger = require('../../utils/log/log.js');
 
 /**
  * Get all availibe ids from all graphs in database
  */
 function getGraphIds(request, response){
-    collection.distinct( '_id',(error, result) => {
+    collection.find().toArray(function(error, result) {
         if (error) {
             logger.error(`${error}`);
             return response.status(500).send(error);
         }
         logger.debug(`find: ${result}`);
-        response.send(result);
+
+        response.send(getIdsAndVariants(result));
     });
+}
+
+function getIdsAndVariants(result){
+    let idVariantsCount = [];
+    for(let x of result){
+        let id = x["_id"];
+        let variants = x["dfg"]["graph"][0]["data"]["variants"];
+        let variantsCount = Object.keys(variants).length;
+        idVariantsCount.push({"_id": id, "variantsCount": variantsCount});
+    }
+    return idVariantsCount;
 }
 
 module.exports = getGraphIds;
