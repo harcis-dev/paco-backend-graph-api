33ff066fe92c578a7bc5f7118bff58440812dfb2
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2021-11-14 19:08:01 +0100

    Add variants to graphml

--- src/utils/convert/dfgToGraphml.js
+++ src/utils/convert/dfgToGraphml.js
@@ -13,16 +13,20 @@ function convertDFG2Graphml(graphJSONID, dfg) {
     xmlString += `<graph id="${graphId}" edgedefault="directed">`
     for (var i = 0; i < dfg.length; i++) {
         let graphData = dfg[i]["data"];
+        let variants = JSON.stringify(graphData["variants"]);
         if (!graphData.hasOwnProperty("target")){
             let id = graphData["id"];
-            let label = graphData["label"].split("\n");;
+            let label = graphData["label"].split("\n");;            
             xmlString += `<node id="${id}">\n`
             xmlString += `<data key="label">${label[0]}</data>\n`
+            xmlString += `<data key="variants">${variants}</data>\n`
             xmlString += `</node>`
         }else{
             let source = graphData["source"];
             let target = graphData["target"];
-            xmlString += `<edge id="${i}" source="${source}" target="${target}"/>`
+            xmlString += `<edge id="${i}" source="${source}" target="${target}">`
+            xmlString += `<data key="variants">${variants}</data>\n`
+            xmlString += `</edge>`
         }
     }
     xmlString += `</graph></graphml>`
@@ -34,6 +38,8 @@ const xmlHead = `<?xml version="1.0" encoding="UTF-8"?>
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
     http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-    <key attr.name="nodeData" attr.type="string" for="node" id="label"/>`
+    <key attr.name="label" attr.type="string" for="node" id="label"/>
+    <key attr.name="variants" attr.type="string" for="node" id="variants" />
+    <key attr.name="variants" attr.type="string" for="edge" id="variants" />`
 
 module.exports = convertDFG2Graphml;
