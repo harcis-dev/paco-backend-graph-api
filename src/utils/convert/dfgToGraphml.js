a8fb316669389cc384a8c7a4fb96a72f30f0a7ac
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2021-11-21 14:30:55 +0100

    Update download with and without variants

--- src/utils/convert/dfgToGraphml.js
+++ src/utils/convert/dfgToGraphml.js
@@ -9,23 +9,33 @@ const logger = require('../log/log.js');
 function convertDFG2Graphml(graphJSONID, dfg) {
     logger.debug(`convert dfg to graphml`);
     let xmlString = xmlHead;
+    
+    if (dfg[0]["data"].hasOwnProperty("variants")){
+        xmlString +='<key attr.name="variants" attr.type="string" for="node" id="variants" />\
+        <key attr.name="variants" attr.type="string" for="edge" id="variants" />'
+    }
     let graphId = graphJSONID
     xmlString += `<graph id="${graphId}" edgedefault="directed">`
     for (var i = 0; i < dfg.length; i++) {
         let graphData = dfg[i]["data"];
+        hasVariants = graphData.hasOwnProperty("variants");
         let variants = JSON.stringify(graphData["variants"]);
         if (!graphData.hasOwnProperty("target")){
             let id = graphData["id"];
             let label = graphData["label"].split("\n");;            
             xmlString += `<node id="${id}">\n`
             xmlString += `<data key="label">${label[0]}</data>\n`
-            xmlString += `<data key="variants">${variants}</data>\n`
+            if (hasVariants){
+                xmlString += `<data key="variants">${variants}</data>\n`
+            }
             xmlString += `</node>`
         }else{
             let source = graphData["source"];
             let target = graphData["target"];
             xmlString += `<edge id="${i}" source="${source}" target="${target}">`
-            xmlString += `<data key="variants">${variants}</data>\n`
+            if (hasVariants){
+                xmlString += `<data key="variants">${variants}</data>\n`
+            }
             xmlString += `</edge>`
         }
     }
@@ -38,8 +48,6 @@ const xmlHead = `<?xml version="1.0" encoding="UTF-8"?>
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
     http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-    <key attr.name="label" attr.type="string" for="node" id="label"/>
-    <key attr.name="variants" attr.type="string" for="node" id="variants" />
-    <key attr.name="variants" attr.type="string" for="edge" id="variants" />`
+    <key attr.name="label" attr.type="string" for="node" id="label"/>`
 
 module.exports = convertDFG2Graphml;
