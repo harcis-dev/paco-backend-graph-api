cf3edcd26a6bf66e72b0ca1ade27ee20ec0f3d36
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2021-12-12 14:56:37 +0100

    Update grapdIds - check if graph exist

--- src/controller/crud/graphIds.js
+++ src/controller/crud/graphIds.js
@@ -1,4 +1,3 @@
-
 const logger = require('../../utils/log/log.js');
 
 /**
@@ -7,7 +6,7 @@ const logger = require('../../utils/log/log.js');
  * - graph name
  * - variantscount
  */
-function getGraphIds(request, response){
+function getGraphIds(request, response) {
     collection.find().toArray(function(error, result) {
         if (error) {
             logger.error(`${error}`);
@@ -24,21 +23,40 @@ function getGraphIds(request, response){
  * @param {*} result 
  * @returns 
  */
-function getIdsAndVariants(result){
+function getIdsAndVariants(result) {
     let idVariantsCount = [];
-    for(let x of result){
-        let id = x["_id"];
-        let name = x["name"];
-        if(x["dfg"]["graph"][0]["data"].hasOwnProperty("variants")){
-            let variants = x["dfg"]["graph"][0]["data"]["variants"];
-            let variantsCount = Object.keys(variants).length;
-            idVariantsCount.push({"_id": id, "name": name, "variantsCount": variantsCount});
-        }else{
-            idVariantsCount.push({"_id": id, "name": name});
+    for (let graphs of result) {
+        let id = graphs["_id"];
+        let name = graphs["name"];
+        let graph = 0;
+        let variantsCount = 0;
+
+        if (graphs.hasOwnProperty("dfg")) {
+            graph = graphs["dfg"];
+        } else if (graphs.hasOwnProperty("epc")) {
+            graph = graphs["epc"];
+        } else if (graphs.hasOwnProperty("bpmn")) {
+            graph = graphs["bpmn"];
         }
-        
-        
-        
+
+        if (graph != 0) {
+            if (graph["graph"][0]["data"].hasOwnProperty("variants")) {
+                let variants = graph["graph"][0]["data"]["variants"];
+                variantsCount = Object.keys(variants).length;
+            }
+            idVariantsCount.push({
+                "_id": id,
+                "name": name,
+                "variantsCount": variantsCount
+            });
+        } else {
+            idVariantsCount.push({
+                "_id": "-1",
+                "name": "Graph does not exist",
+                "variantsCount": -1
+            });
+        }
+
     }
     return idVariantsCount;
 }
