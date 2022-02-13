b0807642d32f0ca995f33b68291b38daf28fe2ad
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2022-02-13 14:51:13 +0100

    check csv header and first row

--- src/controller/csv/import/csvImport.js
+++ src/controller/csv/import/csvImport.js
@@ -29,6 +29,56 @@ function csvImport(request, response) {
         "path": `${file_path}`
     }
 
+    const data = fs.readFileSync(file_path, 'UTF-8');    
+    const rows = data.split('\n');
+
+    let headerColumns = rows[0].split(',');
+    for (let i = 0; i < headerColumns.length; i++) {
+        headerColumns[i] = headerColumns[i].replace(/[^a-zA-Z0-9]/g, '');
+    }
+
+    if(!headerColumns.includes('caseid')){
+        let errorMessage = 'Table need "case id" column';
+        fs.unlinkSync(file_path);
+        logger.error(errorMessage);
+        return response.status(500).send(errorMessage);
+    }
+
+    let caseIndex = headerColumns.indexOf('caseid');
+
+    let columns = rows[1].split(',');
+    let caseID = columns[caseIndex];
+    if(!caseID.match(/^[0-9]+_[0-9]+$/)){
+        let errorMessage = `"case id" does not match pattern "^[0-9]+_[0-9]+$": ${caseID}`;
+        fs.unlinkSync(file_path);
+        logger.error(errorMessage);
+        return response.status(500).send(errorMessage);
+    }
+
+    if(!headerColumns.includes('activity')){
+        let errorMessage = 'Table needs "activity" column';
+        fs.unlinkSync(file_path);
+        logger.error(errorMessage);
+        return response.status(500).send(errorMessage);
+    }
+
+    if(!headerColumns.includes('timestamp')){
+        let errorMessage = 'Table needs "timestamp" column';
+        fs.unlinkSync(file_path);
+        logger.error(errorMessage);
+        return response.status(500).send(errorMessage);
+    }
+
+    let timestampIndex = headerColumns.indexOf('timestamp');
+
+    let timestamp = columns[timestampIndex];
+    if(!(new Date(timestamp)).getTime() > 0){
+        let errorMessage = `timestamp not valid: ${timestamp}`;
+        fs.unlinkSync(file_path);
+        logger.error(errorMessage);
+        return response.status(500).send(errorMessage);
+    }
+
     let database = mongodb.getDatabase();
     database.collection(mongodb.mongodbCsvCollection).insertOne(object, (error, result) => {
         if (error) {
@@ -40,5 +90,7 @@ function csvImport(request, response) {
         response.send(result);
     });
 }
+
+
 
 module.exports = csvImport;
